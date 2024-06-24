1f8c95bc1ab6743699607ab4158d7619766ee15d
m0nolith88 <88949402+m0nolith88@users.noreply.github.com>
2024-06-24 20:49:25 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,65 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <limits.h>
 
-#define LIST 27
+#define LIST 4
 
-int frequency[]={5,9,12,13,16,45};
-char characters[]={'a','b','c','d','e','f'};
+char symbols[LIST]={'A','B','C','D'};
+int p[LIST]={1,2,3,4};
 
-struct node {
+struct node{
     char symbol;
-    int probabylity;
+    int p;
     struct node *left;
     struct node *right;
 };
 
-void createHuffman();
-int findSmallest(struct node *array[LIST],int num);
+struct heap{
+    int p;
+    struct node *left;
+    struct node *right;
+};
 
-//Return smallest in array leaf index
+struct heap * buildHuffman();
+int findSmallest(struct node *array[LIST],int num);
+void list(struct node *array[LIST]);
+
+struct heap * buildHuffman(){
+    struct node *array[LIST]; int n=LIST; struct node *temp;
+    int st,nd; struct node *temp2;
+    for(int i=0; i<LIST; i++){
+        array[i]=(struct node *) malloc(sizeof(struct node));
+        array[i]->p=p[i];
+        array[i]->symbol=symbols[i];
+        array[i]->left=array[i]->right=NULL;
+    }
+    while(n>1){
+        st=findSmallest(array,-1);
+        temp=array[st];
+        nd=findSmallest(array,st);
+        temp2=array[nd];
+        array[st]->p+=array[nd]->p;
+        array[st]->symbol=127;
+        array[st]->left=temp;
+        array[st]->right=temp2;
+        array[nd]->p=-1;
+        --n;
+    }
+
+}
+
 int findSmallest(struct node *array[LIST],int num){
-    int i; int smallest=array[0]; int sIndex;
-    for(i=0; i<LIST; i++){
-        if(array[i]->probabylity<smallest && array[i]->probabylity!=num){
-            smallest=array[i]->probabylity;
+    int smallest=INT_MAX; int sIndex;
+    struct node * temp=(struct node *) malloc(sizeof(struct node));
+    for(int i=0; i<LIST; i++){
+        if(array[i]->p<smallest && array[i]->p!=-1 && i!=num){
+            smallest=array[i]->p;
             sIndex=i;
         }
     }
     return sIndex;
+};
+
+void list(struct node *array[LIST]){
+    for(int i=0; i<LIST; i++){
+        if(array[i]!=NULL){
+            printf("%c %d\n",array[i]->symbol,array[i]->p);
+        }
+    }
 }
 
-void createHuffman(){
-    int i=0; int firstSmallest,sndSmallest; int n=6;
-    struct node *array[LIST];
-    //List creation
-    for(i=0; i<LIST; i++){
-        array[i]=(struct node *) malloc(sizeof(struct node));
-        array[i]->left=array[i]->right=NULL;
-        array[i]->probabylity=frequency[i];
-        array[i]->symbol=characters[i];
-    }
-    while(n>1){
-        firstSmallest=findSmallest(array,-1);
-        sndSmallest=findSmallest(array,array[firstSmallest]->probabylity);
-        struct node *tmp=array[firstSmallest];
-        array[firstSmallest]=(struct node *) malloc(sizeof(struct node));
-        array[firstSmallest]->probabylity=array[sndSmallest]->probabylity+tmp->probabylity;
-        array[firstSmallest]->symbol='*';
-        array[firstSmallest]->right=array[sndSmallest];
-        array[firstSmallest]->left=tmp;
-        array[sndSmallest]->probabylity=-1;
-        n--;
-        printf("%d\n",tmp->probabylity);
-    }
-    printf("%c\n",array[firstSmallest]->symbol);
-}
-
-
-int main()
-{
-    createHuffman();
-//    struct node * smallest = find2smallest();
-    //printf("Probability:%d Character:%c\n",array->probabylity,array->symbol);
+int main(int argc,char *argv[]){
+    buildHuffman();
     return 0;
 }
